aad54b6e4c2514bb8e2854337b99344e8f1ed0b5
elmahdim <elmahdi.mahmoud@live.com>
2017-04-26 15:37:48 +0300

    create app file, refactor index files

---
+++ src/app.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {
+    View,
+    Text
+} from 'react-native';
+
+const App = () => {
+    return (
+      <View>
+          <Text>Hello from the other side!</Text>
+      </View>
+    );
+};
+
+export default App;
